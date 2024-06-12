4bb73a07b183efd630e4b1d020e28940452a69b5
Polaris-algedi <karchalmohammed@gmail.com>
2024-06-12 05:46:50 +0100

    refactor: Use react-query to fetch pet details in Details component

--- Complete-react-v8/adopt-me/src/Details.jsx
+++ Complete-react-v8/adopt-me/src/Details.jsx
@@ -1,9 +1,37 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 export default function Details() {
-  const params = useParams();
   const { id } = useParams();
-  console.log(params);
+  const { isLoading, /* isError, */ data } = useQuery(
+    ["details", id],
+    fetchPet,
+  );
 
-  return <h2>{id}</h2>;
+  if (isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🐶</h2>
+      </div>
+    );
+  }
+
+  //if (isError) return <h1>There is an Error</h1>;
+
+  const pet = data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        <button onClick={() => window.history.back()}>Go Back</button>
+      </div>
+    </div>
+  );
 }
